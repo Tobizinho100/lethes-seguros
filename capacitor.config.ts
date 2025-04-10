9eceead159d7a646741d6abe7551ff04ea23a7a5
Tobi <149004951+Tobizinho100@users.noreply.github.com>
2025-04-10 10:32:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lethes-seguros',
+  webDir: 'dist'
+};
+
+export default config;
